babe14a4879be5c60a0c29c1a3b5e20d6090a3f4
BaduTouch <matildaefyabadu@gmail.com>
2023-03-24 00:49:45 +0000

    First script

---
+++ adduser.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#purpose= this script is designed to add a new user
+#start
+echo "Welcome $USER"
+echo "This script will add a new user and assign password"
+echo "What is the name of your new  user"
+read -p "Please enter the new user name here" newuser
